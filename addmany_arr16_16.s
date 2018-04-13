6c848038578ab607efb5d7c88d10de0e5d4d0502
a-lula <adamlula99@gmail.com>
2018-04-13 12:28:57 -0400

    Adds all the elements of 2 byte array
    Takes two arguments UBYTE,UINT16*
    where UBYTE is the size of the array

---
+++ addmany_arr16_16.s
@@ -0,0 +1,47 @@
+    .globl _sumarray
+    _sumarray:         
+    LDA  HL,2(SP)
+    LD   A,(HL)
+    INC  HL
+	
+	LD   D,(HL)
+    INC  HL
+	LD	 H,(HL)
+	LD	 L,D
+	
+	
+	LD	 E,(HL)
+	INC	 HL
+	LD   D,(HL)
+	INC  HL
+	
+	PUSH DE
+	
+	JP   whileloop
+	
+	whileloop:
+
+	CP 	 #1
+	JP   Z,END
+	DEC  A
+
+	LD	 C,(HL)
+	INC  HL
+	LD	 B,(HL)
+	INC  HL
+	
+	POP	 DE
+	PUSH HL
+	LD   H,D
+	LD	 L,E
+	
+    ADD  HL,BC
+	LD	 D,H
+	LD	 E,L
+	POP  HL
+	PUSH DE
+	JP   whileloop
+
+	END:
+	POP  DE
+	RET
---
+++ addmany_arr8_8.s
@@ -0,0 +1,46 @@
+    .globl _sumarray
+    _sumarray:         
+    LDA  HL,2(SP)
+    LD   A,(HL)
+    INC  HL
+	
+	LD   D,(HL)
+    INC  HL
+	LD	 H,(HL)
+	LD	 L,D
+	
+	LD	 E,(HL)
+	INC	 HL
+	LD   D,(HL)
+	PUSH DE
+	INC  HL
+	JP   whileloop
+	
+	whileloop:
+
+	CP 	 #1
+	JP   Z,END
+	DEC  A
+	
+
+	LD	 C,(HL)
+	INC  HL
+	LD	 B,(HL)
+	INC  HL
+	
+	POP	 DE
+	PUSH HL
+	LD   H,D
+	LD	 L,E
+	
+    ADD  HL,DE
+	MOV	 D,H
+	MOV	 E,L
+	POP  HL
+	PUSH DE
+	JP   whileloop
+
+	END:
+	# LD  D,#0
+	POP  DE
+	RET
